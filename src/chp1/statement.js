67b7dcf4f515d2d9a7980dedbcd1dfca9fbed638
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-01-22 12:04:11 +0000

    extract format

--- src/chp1/statement.js
+++ src/chp1/statement.js
@@ -1,6 +1,14 @@
 export {statement}
 
 
+function format(aNumber) {
+    return new Intl.NumberFormat("en-US",
+        {
+            style: "currency", currency: "USD",
+            minimumFractionDigits: 2
+        }).format(aNumber);
+}
+
 function statement(invoice, plays) {
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
@@ -40,11 +48,6 @@ function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        {
-            style: "currency", currency: "USD",
-            minimumFractionDigits: 2
-        }).format;
 
 
     for (let perf of invoice.performances) {
