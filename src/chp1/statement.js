cdee0d539623224ab79af3cfcea84fc2cee06bd3
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-01-22 12:16:03 +0000

    move / 100 to usd

--- src/chp1/statement.js
+++ src/chp1/statement.js
@@ -6,7 +6,7 @@ function usd(aNumber) {
         {
             style: "currency", currency: "USD",
             minimumFractionDigits: 2
-        }).format(aNumber);
+        }).format(aNumber/ 100);
 }
 
 function statement(invoice, plays) {
@@ -55,10 +55,10 @@ function statement(invoice, plays) {
         volumeCredits += volumeCreditsFor(perf);
 
         // print line for this order
-        result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+        result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${usd(totalAmount / 100)}\n`;
+    result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 }
