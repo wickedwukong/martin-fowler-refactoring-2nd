ab109ba7e3157e115d59a601692af55dfab6b988
Xuemin Guan <Xuemin Guan>
2019-01-22 21:33:41 +0000

    move amount into subclass - tragegy and comedy caculators

--- src/chp1/createStatementData.js
+++ src/chp1/createStatementData.js
@@ -14,28 +14,37 @@ class PerformanceCalculator {
         if ("comedy" === this.play.type) volumeCredits += Math.floor(this.performance.audience / 5);
         return volumeCredits
     }
+}
 
+class TragedyCalculator extends PerformanceCalculator {
     get amount() {
-        let result = 0;
-        switch (this.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${this.play.type}`);
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
         }
         return result;
+    }
+}
 
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+    switch (aPlay.type) {
+        case "tragedy":
+            return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy" :
+            return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`unknown type: ${aPlay.type}`);
     }
 }
 
@@ -48,13 +57,14 @@ function createStatementData(invoice, plays) {
     return statementData;
 
     function enhancePerformance(aPerformance) {
-        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play
         result.amount = calculator.amount;
         result.volumeCredits = calculator.volumeCredits;
         return result;
     }
+
 
     function totalVolumeCredits(statementData) {
         return statementData.performances.reduce((total, performance) => total + performance.volumeCredits, 0)
