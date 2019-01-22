5b96a808cee3ce3b8f0777b70ea47690c9fb2ef4
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-01-22 12:09:24 +0000

    rename format to usd

--- src/chp1/statement.js
+++ src/chp1/statement.js
@@ -1,7 +1,7 @@
 export {statement}
 
 
-function format(aNumber) {
+function usd(aNumber) {
     return new Intl.NumberFormat("en-US",
         {
             style: "currency", currency: "USD",
@@ -55,10 +55,10 @@ function statement(invoice, plays) {
         volumeCredits += volumeCreditsFor(perf);
 
         // print line for this order
-        result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+        result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `Amount owed is ${usd(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 }
