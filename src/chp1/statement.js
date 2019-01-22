ac78601846cdcf5181eaf8b0af1d02cd1575fba4
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-01-22 13:47:12 +0000

    split loop

--- src/chp1/statement.js
+++ src/chp1/statement.js
@@ -46,18 +46,20 @@ function statement(invoice, plays) {
 
 
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
 
 
     for (let perf of invoice.performances) {
-        // add volume credits
-        volumeCredits += volumeCreditsFor(perf);
-
         // print line for this order
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
+
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
+    }
+    
     result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
