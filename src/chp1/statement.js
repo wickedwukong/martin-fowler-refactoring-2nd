a1d926fbe676e7a5788ccd55c9d892df5df290a3
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-01-22 17:27:48 +0000

    add htmlStatement

--- src/chp1/statement.js
+++ src/chp1/statement.js
@@ -1,4 +1,5 @@
 export {statement}
+export {htmlStatement}
 
 import {createStatementData} from './createStatementData.js'
 
@@ -22,7 +23,23 @@ function renderPlainText(statementData) {
     return result;
 }
 
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
 
+function renderHtml(data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+    for (let perf of data.performances) {
+        result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+    return result;
+}
 
 function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
