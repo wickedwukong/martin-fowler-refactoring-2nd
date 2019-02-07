b3ef5a67e0e6a54989fc4d5c7367da8474a2bbed
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-02-07 15:53:46 +0000

    chp 6.3.1 | Extract Variable | adding tests for prices

---
+++ src/chp6/ExtractVarialbe1.js
@@ -0,0 +1,6 @@
+export function price(order) {
+    //price is base price - quantity discount + shipping
+    return order.quantity * order.itemPrice -
+        Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+        Math.min(order.quantity * order.itemPrice * 0.1, 100);
+}
---
+++ test/chp6/ExtractVarialbe1.test.js
@@ -0,0 +1,25 @@
+import { expect } from 'chai';
+import {price} from '../../src/chp6/ExtractVarialbe1'
+
+describe('price', () => {
+    it('should price order which quantity is under 50', () => {
+        const anOrder = {
+            quantity: 49,
+            itemPrice: 1
+        };
+
+        expect(price(anOrder)).to.equal(53.9);
+    });
+
+    it('should price order which quantity is above 50', () => {
+        const anOrder = {
+            quantity: 51,
+            itemPrice: 1
+        };
+
+        expect(price(anOrder)).to.equal(56.1);
+
+    });
+
+
+});
