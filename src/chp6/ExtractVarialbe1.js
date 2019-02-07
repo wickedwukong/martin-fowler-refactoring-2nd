1763480f44db3cf40cb5270dd77caaf7214f81fa
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-02-07 15:57:10 +0000

    chp 6.3.1 | Extract Variable | extract shipping

--- src/chp6/ExtractVarialbe1.js
+++ src/chp6/ExtractVarialbe1.js
@@ -1,7 +1,6 @@
 export function price(order) {
-    //price is base price - quantity discount + shipping
     let basePrice = order.quantity * order.itemPrice;
     let quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
-    return basePrice - quantityDiscount +
-        Math.min(basePrice * 0.1, 100);
+    let shippingCost = Math.min(basePrice * 0.1, 100);
+    return basePrice - quantityDiscount + shippingCost;
 }
