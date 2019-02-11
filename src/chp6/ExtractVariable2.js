5bb36c17e2b4ca03dda2c36b797795f011122589
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-02-11 13:48:34 +0000

    chp 6.3.2 | Extract Variable in a class | extract base price as a function

--- src/chp6/ExtractVariable2.js
+++ src/chp6/ExtractVariable2.js
@@ -7,8 +7,12 @@ export class Order {
     get itemPrice() {return this._data.itemPrice;}
 
     get price() {
-        return this.quantity * this.itemPrice -
+        return this.basePrice -
             Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
-            Math.min(this.quantity * this.itemPrice * 0.1, 100);
+            Math.min(this.basePrice * 0.1, 100);
+    }
+
+    get basePrice() {
+        return this.quantity * this.itemPrice;
     }
 }
