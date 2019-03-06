1ef8a37f403d03ba0d2ead1b49830fdb9ae8b153
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-03-06 13:44:33 +0000

    chp 6.5.2 | Change Function Declaration | change all callers to use the new function and supply the isPriority parameter

--- src/chp6/ChangeFunctionDeclaration2.js
+++ src/chp6/ChangeFunctionDeclaration2.js
@@ -2,11 +2,6 @@ export default class Book {
     constructor() {
         this._reservations = [];
     }
-
-    addReservation(customer) {
-        this.zz_addReservation(customer, false);
-    }
-
     zz_addReservation(customer, isPriority) {
         console.assert(isPriority === true || isPriority === false);
         this._reservations.push(customer);
