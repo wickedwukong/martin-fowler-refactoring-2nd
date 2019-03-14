0f8340e9ae066ee0063e741655697e5db9fd46fd
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-03-14 13:09:59 +0000

    chp 6.5.4.1 | Encapsulate Variable - default user | adding getter and setter to encapsulate fields

--- src/chp6/Encapsulatevariable1.js
+++ src/chp6/Encapsulatevariable1.js
@@ -1,1 +1,4 @@
-export let defaultOwner = {firstName: "Martin", lastName: "Fowler"};
+let defaultOwner = {firstName: "Martin", lastName: "Fowler"};
+
+export function getDefaultOwner()    {return defaultOwner;}
+export function setDefaultOwner(arg) {defaultOwner = arg;}
--- test/chp6/Encapsulatevariable1.test.js
+++ test/chp6/Encapsulatevariable1.test.js
@@ -1,13 +1,22 @@
 import { expect } from 'chai';
-import {defaultOwner} from '../../src/chp6/Encapsulatevariable1'
+import {getDefaultOwner, setDefaultOwner} from '../../src/chp6/Encapsulatevariable1'
 
 describe('defaultOwner', () => {
     it('is Martin Folwer', () => {
         let spaceship = {};
-        spaceship.owner = defaultOwner;
+        spaceship.owner = getDefaultOwner();
 
         expect(spaceship.owner.firstName).to.equal("Martin");
         expect(spaceship.owner.lastName).to.equal("Fowler");
+    });
+
+    it('can be set', () => {
+        setDefaultOwner({firstName: "Mike", lastName: "Jackson"})
+
+        let newDefaultUser = getDefaultOwner()
+
+        expect(newDefaultUser.firstName).to.equal("Mike");
+        expect(newDefaultUser.lastName).to.equal("Jackson");
     });
 
 });
