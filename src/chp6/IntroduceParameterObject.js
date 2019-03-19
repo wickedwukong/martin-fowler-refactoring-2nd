21e98a2f907f277bd2c6250bfc53847a97e7b9c6
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-03-19 12:48:27 +0000

    chp 6.6 | Introduce Parameter Object | move comparison logic into the newly create parameter object Range. A new function contains performs comparions

--- src/chp6/IntroduceParameterObject.js
+++ src/chp6/IntroduceParameterObject.js
@@ -1,6 +1,6 @@
 export function readingsOutsideRange(station, range) {
     return station.readings
-        .filter(r => r.temp < range.min || r.temp > range.max);
+        .filter(r => !range.contains(r.temp));
 }
 
 export class NumberRange {
@@ -9,4 +9,7 @@ export class NumberRange {
     }
     get min() {return this._data.min;}
     get max() {return this._data.max;}
+
+    contains(aNumber) {return (aNumber >= this.min && aNumber <= this.max);}
+
 }
