f5f28bd3ad5163d0ef36cb1ee7b83ee00f8cd95e
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-04-09 12:07:05 +0100

    :recycle: chp 6.9 | Split Phase | java | get back to the point where command line in introduced to lay the foundation for refactoring towards a transfomer to enrich CommandLine

--- src/main/java/DisplayOrderCount.java
+++ src/main/java/DisplayOrderCount.java
@@ -21,29 +21,23 @@ public class DisplayOrderCount {
     }
 
     static long run(String[] args) throws java.io.IOException {
-        return countOrders(parseCommandLine(args));
-    }
-
-    private static CommandLine parseCommandLine(String[] args) {
         if (args.length == 0) throw new RuntimeException("must supply a filename");
         CommandLine commandLine = new CommandLine();
         commandLine.filename = args[args.length - 1];
         commandLine.onlyCountReady = Stream.of(args).anyMatch(arg -> "-r".equals(arg));
-        return commandLine;
+
+        return countOrders(commandLine);
     }
 
     private static long countOrders(CommandLine commandLine) throws java.io.IOException {
-        Order[] orders = new ObjectMapper().readValue(readOrderFile(commandLine), Order[].class);
-        
+        File input = Paths.get(commandLine.filename).toFile();
+        ObjectMapper mapper = new ObjectMapper();
+        Order[] orders = mapper.readValue(input, Order[].class);
         if (commandLine.onlyCountReady) {
             return Stream.of(orders).filter(order -> "ready".equals(order.status)).count();
         } else {
             return orders.length;
         }
-    }
-
-    private static File readOrderFile(CommandLine commandLine) {
-        return Paths.get(commandLine.filename).toFile();
     }
 }
 
