74c3f1dae478a4e1fb855c3589da83b8d3bb3614
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-04-09 10:59:00 +0100

    :recycle: chp 6.9 | Split Phase | extract function to cacluate price data so that we have two phases now. This finishes the refacotring of priceOrder

--- src/chp6/SplitPhase1.js
+++ src/chp6/SplitPhase1.js
@@ -7,15 +7,17 @@ function applyingShippingCost(shippingMethod, priceData) {
     return priceData.basePrice - priceData.discount + shippingCost;
 }
 
-export function priceOrder(product, quantity, shippingMethod) {
+function calculatePriceData(product, quantity) {
     const basePrice = product.basePrice * quantity;
 
     const discount = Math.max(quantity - product.discountThreshold, 0)
         * product.basePrice * product.discountRate;
 
-    const priceData = {basePrice: basePrice, quantity: quantity, discount: discount};
+    return {basePrice: basePrice, quantity: quantity, discount: discount};
+}
 
-    const price = applyingShippingCost(shippingMethod, priceData);
+export function priceOrder(product, quantity, shippingMethod) {
+    const priceData = calculatePriceData(product, quantity);
 
-    return price;
+    return applyingShippingCost(shippingMethod, priceData);
 }
