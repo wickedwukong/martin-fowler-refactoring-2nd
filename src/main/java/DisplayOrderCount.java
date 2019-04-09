49fda9693fe3da16d06e2f83eb5f6914541dfad5
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-04-09 11:19:56 +0100

    :recycle: chp 6.9 | Split Phase | java | extract run and countOrders to make it testable

--- src/main/java/DisplayOrderCount.java
+++ src/main/java/DisplayOrderCount.java
@@ -7,18 +7,27 @@ import java.util.stream.Stream;
 public class DisplayOrderCount {
     public static void main(String[] args) {
         try {
-            if (args.length == 0) throw new RuntimeException("must supply a filename");
-            String filename = args[args.length - 1];
-            File input = Paths.get(filename).toFile();
-            ObjectMapper mapper = new ObjectMapper();
-            Order[] orders = mapper.readValue(input, Order[].class);
-            if (Stream.of(args).anyMatch(arg -> "-r".equals(arg)))
-                System.out.println(Stream.of(orders).filter(order -> "ready".equals(order.status)).count());
-            else
-                System.out.println(orders.length);
+            System.out.println(run(args));
         } catch (Exception e) {
             System.err.println(e);
             System.exit(1);
+        }
+    }
+
+    private static long run(String[] args) throws java.io.IOException {
+        if (args.length == 0) throw new RuntimeException("must supply a filename");
+        String filename = args[args.length - 1];
+        return countOrders(args, filename);
+    }
+
+    private static long countOrders(String[] args, String filename) throws java.io.IOException {
+        File input = Paths.get(filename).toFile();
+        ObjectMapper mapper = new ObjectMapper();
+        Order[] orders = mapper.readValue(input, Order[].class);
+        if (Stream.of(args).anyMatch(arg -> "-r".equals(arg))) {
+            return Stream.of(orders).filter(order -> "ready".equals(order.status)).count();
+        } else {
+            return orders.length;
         }
     }
 }
