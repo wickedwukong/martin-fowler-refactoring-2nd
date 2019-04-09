6c4dac12e69e148636f4407ae5e85437a2d4d9f3
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-04-09 10:31:52 +0100

    :recycle: chp 6.9 | Split Phase | extract applying shipping cost

--- src/chp6/SplitPhase1.js
+++ src/chp6/SplitPhase1.js
@@ -1,15 +1,19 @@
+function applyingShippingCost(basePrice, shippingMethod, quantity, discount) {
+    const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
+        ? shippingMethod.discountedFee : shippingMethod.feePerCase;
+
+    const shippingCost = quantity * shippingPerCase;
+
+    return basePrice - discount + shippingCost;
+}
+
 export function priceOrder(product, quantity, shippingMethod) {
     const basePrice = product.basePrice * quantity;
 
     const discount = Math.max(quantity - product.discountThreshold, 0)
         * product.basePrice * product.discountRate;
 
-    const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
-        ? shippingMethod.discountedFee : shippingMethod.feePerCase;
+    const price = applyingShippingCost(basePrice, shippingMethod, quantity, discount);
 
-    const shippingCost = quantity * shippingPerCase;
-
-    const price =  basePrice - discount + shippingCost;
-    
     return price;
 }
