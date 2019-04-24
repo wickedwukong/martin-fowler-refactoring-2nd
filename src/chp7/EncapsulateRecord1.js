736f54322088f8a3f8bb4a78154ffbcd3143adb3
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-04-24 12:52:47 +0100

    chp 7.1 | Provided setter

--- src/chp7/EncapsulateRecord1.js
+++ src/chp7/EncapsulateRecord1.js
@@ -2,6 +2,8 @@ class Organization {
     constructor(data) {
         this._data = data;
     }
+
+    set name(aString) {this._data.name = aString;}
 }
 
 export const organization = new Organization({name: "Acme Gooseberries", country: "GB"});
