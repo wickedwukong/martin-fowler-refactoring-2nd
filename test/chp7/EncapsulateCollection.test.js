837727fe467170b12d1f813c81f6cf7fba5b0725
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-04-29 16:55:57 +0100

    chp 7.2 | add more tests

--- test/chp7/EncapsulateCollection.test.js
+++ test/chp7/EncapsulateCollection.test.js
@@ -1,13 +1,23 @@
-import { expect } from 'chai';
+import {expect} from 'chai';
 import {Person, Course} from '../../src/chp7/EncapsulateCollection'
 
 describe('Person', () => {
     it('can set courses', () => {
-        const person = new Person("Xuemin");
+        const aPerson = new Person("Xuemin");
         const courses = [new Course("Math", true)];
 
-        person.courses = courses;
-        expect(person.name).to.equal("Xuemin");
-        expect(person.courses).to.equal(courses);
+        aPerson.courses = courses;
+        expect(aPerson.name).to.equal("Xuemin");
+        expect(aPerson.courses).to.eql(courses);
+    });
+
+    it('can set courses', () => {
+        const aPerson = new Person("Xuemin");
+
+        aPerson.courses.push(new Course("Math", false));
+        aPerson.courses.push(new Course("Photography", false));
+
+        expect(aPerson.name).to.equal("Xuemin");
+        expect(aPerson.courses).to.eql([new Course("Math", false), new Course("Photography", false)]);
     });
 });
