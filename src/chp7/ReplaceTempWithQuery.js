f6822a4aad269f5642417706e9b974426a79788a
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-01 15:50:47 +0100

    chp 7.4 | ReplaceTempWithQuery | replace temp basePrice with a getter

--- src/chp7/ReplaceTempWithQuery.js
+++ src/chp7/ReplaceTempWithQuery.js
@@ -6,9 +6,12 @@ export class Order {
     }
 
     get price() {
-        var basePrice = this._quantity * this._item.price;
         var discountFactor = 0.98;
-        if (basePrice > 1000) discountFactor -= 0.03;
-        return basePrice * discountFactor;
+        if (this.basePrice > 1000) discountFactor -= 0.03;
+        return this.basePrice * discountFactor;
+    }
+
+    get basePrice() {
+        return this._quantity * this._item.price;
     }
 }
