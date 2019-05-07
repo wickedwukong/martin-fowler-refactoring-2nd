bc719875fd6d6fed7ab74dfa2372a076510bde49
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-07 17:25:30 +0100

    chp 7.8 | Substitute Algorithm | substituted algorithm

--- src/chp7/SubstituteAlgorithm.js
+++ src/chp7/SubstituteAlgorithm.js
@@ -1,14 +1,4 @@
 export function foundPerson(people) {
-    for(let i = 0; i < people.length; i++) {
-        if (people[i] === "Don") {
-            return "Don";
-        }
-        if (people[i] === "John") {
-            return "John";
-        }
-        if (people[i] === "Kent") {
-            return "Kent";
-        }
-    }
-    return "";
+    const candidates = ["Don", "John", "Kent"];
+    return people.find(p => candidates.includes(p)) || '';
 }
