e6bc77b814933d376fea802816ed47fdec2cd55e
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-08 17:09:34 +0100

    chp 8.1.1 | Move Function | remove calculateDistance() and use top_calculateDistance() instead

--- src/chp8/MoveFunction1.js
+++ src/chp8/MoveFunction1.js
@@ -1,34 +1,13 @@
 export function trackSummary(points) {
     const totalTime = calculateTime();
-    const totalDistance = calculateDistance();
+    const totalDistance = top_calculateDistance(points);
     const pace = totalTime / 60 / totalDistance;
     return {
         time: totalTime,
         distance: totalDistance,
         pace: pace
     };
-
-    function calculateDistance() {
-        return top_calculateDistance(points);
-    }
-
-    function distance(p1, p2) {
-        // haversine formula see http://www.movable-type.co.uk/scripts/latlong.html
-        const EARTH_RADIUS = 3959; // in miles
-        const dLat = radians(p2.lat) - radians(p1.lat);
-        const dLon = radians(p2.lon) - radians(p1.lon);
-        const a = Math.pow(Math.sin(dLat / 2), 2)
-            + Math.cos(radians(p2.lat))
-            * Math.cos(radians(p1.lat))
-            * Math.pow(Math.sin(dLon / 2), 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        return EARTH_RADIUS * c;
-    }
-
-    function radians(degrees) {
-        return degrees * Math.PI / 180;
-    }
-
+    
     function calculateTime() {
         //just a hard code number
         return 10000;
