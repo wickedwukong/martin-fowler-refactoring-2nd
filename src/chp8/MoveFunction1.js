213aa60eee567062555fead4a210c03543786ec6
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-08 16:49:24 +0100

    chp 8.1.1 | Move Function | added tests

---
+++ src/chp8/MoveFunction1.js
@@ -0,0 +1,40 @@
+export function trackSummary(points) {
+    const totalTime = calculateTime();
+    const totalDistance = calculateDistance();
+    const pace = totalTime / 60 / totalDistance;
+    return {
+        time: totalTime,
+        distance: totalDistance,
+        pace: pace
+    };
+
+    function calculateDistance() {
+        let result = 0;
+        for (let i = 1; i < points.length; i++) {
+            result += distance(points[i - 1], points[i]);
+        }
+        return result;
+    }
+
+    function distance(p1, p2) {
+        // haversine formula see http://www.movable-type.co.uk/scripts/latlong.html
+        const EARTH_RADIUS = 3959; // in miles
+        const dLat = radians(p2.lat) - radians(p1.lat);
+        const dLon = radians(p2.lon) - radians(p1.lon);
+        const a = Math.pow(Math.sin(dLat / 2), 2)
+            + Math.cos(radians(p2.lat))
+            * Math.cos(radians(p1.lat))
+            * Math.pow(Math.sin(dLon / 2), 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return EARTH_RADIUS * c;
+    }
+
+    function radians(degrees) {
+        return degrees * Math.PI / 180;
+    }
+
+    function calculateTime() {
+        //just a hard code number
+        return 10000;
+    }
+}
---
+++ test/chp8/MoveFunction1.test.js
@@ -0,0 +1,23 @@
+import {expect} from 'chai';
+import {trackSummary} from '../../src/chp8/MoveFunction1'
+
+describe('trackSummary', () => {
+    it('calculate distance, pace and time between two points', () => {
+        const newYork = {
+            lat: 40.73061,
+            lon: -73.935242
+        };
+
+        const tokyo = {
+            lat: 35.652832,
+            lon: 139.839478
+        };
+
+        expect(trackSummary([newYork, tokyo])).to.eql({
+            distance: 6740.9149271449005,
+            pace: 0.024724635819912053,
+            time: 10000
+        });
+    });
+
+});
