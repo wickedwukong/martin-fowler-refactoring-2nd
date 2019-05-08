31469b3b1325b76744c3b9c8c1b921d35820860d
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-08 17:00:00 +0100

    chp 8.1.1 | Move Function | have top_calcualteDisatnace() on the top level together with its dependencies as nested functions

--- src/chp8/MoveFunction1.js
+++ src/chp8/MoveFunction1.js
@@ -38,3 +38,29 @@ export function trackSummary(points) {
         return 10000;
     }
 }
+
+
+function top_calculateDistance(points) {
+    let result = 0;
+    for (let i = 1; i < points.length; i++) {
+        result += distance(points[i - 1], points[i]);
+    }
+    return result;
+
+    function distance(p1, p2) {
+        // haversine formula see http://www.movable-type.co.uk/scripts/latlong.html
+        const EARTH_RADIUS = 3959; // in miles
+        const dLat = radians(p2.lat) - radians(p1.lat);
+        const dLon = radians(p2.lon) - radians(p1.lon);
+        const a = Math.pow(Math.sin(dLat / 2), 2)
+            + Math.cos(radians(p2.lat))
+            * Math.cos(radians(p1.lat))
+            * Math.pow(Math.sin(dLon / 2), 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return EARTH_RADIUS * c;
+    }
+
+    function radians(degrees) {
+        return degrees * Math.PI / 180;
+    }
+}
