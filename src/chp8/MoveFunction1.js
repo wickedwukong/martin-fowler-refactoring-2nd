42408052912a026a5984de247e7999d41a75ae33
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-08 17:28:27 +0100

    chp 8.1.1 | Move Function | move nested functions to top level as 1. JS has other ways of hiding visibility 2. they too easily set up hidden data interrelationships that can get hard to follow.

--- src/chp8/MoveFunction1.js
+++ src/chp8/MoveFunction1.js
@@ -19,21 +19,21 @@ function totalDistance(points) {
         result += distance(points[i - 1], points[i]);
     }
     return result;
+}
 
-    function distance(p1, p2) {
-        // haversine formula see http://www.movable-type.co.uk/scripts/latlong.html
-        const EARTH_RADIUS = 3959; // in miles
-        const dLat = radians(p2.lat) - radians(p1.lat);
-        const dLon = radians(p2.lon) - radians(p1.lon);
-        const a = Math.pow(Math.sin(dLat / 2), 2)
-            + Math.cos(radians(p2.lat))
-            * Math.cos(radians(p1.lat))
-            * Math.pow(Math.sin(dLon / 2), 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        return EARTH_RADIUS * c;
-    }
+function distance(p1, p2) {
+    // haversine formula see http://www.movable-type.co.uk/scripts/latlong.html
+    const EARTH_RADIUS = 3959; // in miles
+    const dLat = radians(p2.lat) - radians(p1.lat);
+    const dLon = radians(p2.lon) - radians(p1.lon);
+    const a = Math.pow(Math.sin(dLat / 2), 2)
+        + Math.cos(radians(p2.lat))
+        * Math.cos(radians(p1.lat))
+        * Math.pow(Math.sin(dLon / 2), 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return EARTH_RADIUS * c;
+}
 
-    function radians(degrees) {
-        return degrees * Math.PI / 180;
-    }
+function radians(degrees) {
+    return degrees * Math.PI / 180;
 }
