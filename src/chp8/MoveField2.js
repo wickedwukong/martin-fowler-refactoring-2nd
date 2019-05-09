c28a0e8cb7de2ea8c6545764fc4d9005ef6c365c
Xuemin Guan <Xuemin Guan>
2019-05-09 14:47:43 +0100

    chp 8.2.2 | Move Filed | add assertion
    To make sure the interest rate in account is same as the one in account type.

--- src/chp8/MoveField2.js
+++ src/chp8/MoveField2.js
@@ -3,6 +3,7 @@ class Account {
     constructor(number, type, interestRate) {
         this._number = number;
         this._type = type;
+        assert(interestRate === this._type.interestRate);
         this._interestRate = interestRate;
     }
 
