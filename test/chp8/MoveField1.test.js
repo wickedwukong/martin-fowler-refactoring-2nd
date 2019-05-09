9676646ad3d8bd7a26e170045c52c92798f90a7a
Xuemin Guan <Xuemin Guan>
2019-05-09 13:34:29 +0100

    chp 8.2.1 | Move Filed | Use import instead of require
    Use ES6 syntax.

--- test/chp8/MoveField1.test.js
+++ test/chp8/MoveField1.test.js
@@ -1,9 +1,9 @@
 import {expect} from 'chai';
 import {Customer} from '../../src/chp8/MoveField1'
+import Money from 'js-money'
 
 describe('Customer', () => {
     it('calculates discount', () => {
-        var Money = require('js-money');
         const customer = new Customer("Mike", 0.05);
         expect(customer.applyDiscount(new Money(500, Money.EUR))).to.eql(new Money(475, Money.EUR));
     });
