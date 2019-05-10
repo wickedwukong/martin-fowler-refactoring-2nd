30896d9057f1d968b76d6573a715bd94844d19e4
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-10 10:39:55 +0100

    chp 8.2.3 | Move Statements into Function
    added tests

---
+++ src/chp8/MoveStatementsIntoFunction.js
@@ -0,0 +1,29 @@
+export function renderPerson(person) {
+    const result = [];
+    result.push(`<p>${person.name}</p>`);
+    result.push(renderPhoto(person.photo));
+    result.push(`<p>title: ${person.photo.title}</p>`);
+    result.push(emitPhotoData(person.photo));
+    return result.join("\n");
+}
+
+
+export function photoDiv(p) {
+    return [
+        "<div>",
+        `<p>title: ${p.title}</p>`,
+        emitPhotoData(p),
+        "</div>",
+    ].join("\n");
+}
+
+function emitPhotoData(aPhoto) {
+    const result = [];
+    result.push(`<p>location: ${aPhoto.location}</p>`);
+    result.push(`<p>date: ${aPhoto.date.toDateString()}</p>`);
+    return result.join("\n");
+}
+
+function renderPhoto(aPhoto) {
+    return "";
+}
---
+++ test/chp8/MoveStatementsIntoFunction.test.js
@@ -0,0 +1,24 @@
+import {expect} from 'chai';
+import {photoDiv, renderPerson} from '../../src/chp8/MoveStatementsIntoFunction'
+
+const aPerson = {
+    name: "Alex",
+    photo: {
+        title: "holiday",
+        location: "Greece",
+        date: new Date()
+    }
+};
+
+describe('photoDiv', () => {
+
+    it('render a photo div', () => {
+        expect(photoDiv(aPerson.photo)).to.equal('<div>\n<p>title: holiday</p>\n<p>location: Greece</p>\n<p>date: Fri May 10 2019</p>\n</div>');
+    });
+});
+
+describe('renderPerson', () => {
+    it('has bankCharge of 4.5 when daysOverdrawn is 0', () => {
+        expect(renderPerson(aPerson)).to.equal('<p>Alex</p>\n\n<p>title: holiday</p>\n<p>location: Greece</p>\n<p>date: Fri May 10 2019</p>');
+    });
+});
