679aa02bba6b5bfbfac0064ca0989e940789607c
Xuemin Guan <Xuemin Guan>
2019-05-13 10:39:12 +0100

    chp 8.2.4 | Move Statements to Callers | added the code

---
+++ src/chp8/MoveStatementsToCallers.js
@@ -0,0 +1,30 @@
+function renderPerson(outStream, person) {
+    outStream.write(`<p>${person.name}</p>\n`);
+    renderPhoto(outStream, person.photo);
+    emitPhotoData(outStream, person.photo);
+}
+
+function listRecentPhotos(outStream, photos) {
+    photos
+        .filter(p => p.date > recentDateCutoff())
+        .forEach(p => {
+            outStream.write("<div>\n");
+            emitPhotoData(outStream, p);
+            outStream.write("</div>\n");
+        });
+}
+
+function emitPhotoData(outStream, photo) {
+    outStream.write(`<p>title: ${photo.title}</p>\n`);
+    outStream.write(`<p>date: ${photo.date.toDateString()}</p>\n`);
+    outStream.write(`<p>location: ${photo.location}</p>\n`);
+}
+
+function renderPhoto(outStream, aPhoto) {
+    outStream.write("");
+}
+
+function recentDateCutoff() {
+    //7 days ago.
+    return new Date().setDate(new Date().getDate() - 7);
+}
