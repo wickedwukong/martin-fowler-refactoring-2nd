3e5a7625046e657927da45c5c8642430d5ffe0a9
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-15 14:51:16 +0100

    chp 8.7 | Split loop | First step, copy the loop, then remove the duplication that would otherwise produce wrong results

--- src/chp8/SplitLoop.js
+++ src/chp8/SplitLoop.js
@@ -1,9 +1,13 @@
 export function reportYoungestAgeAndTotalSalary(people) {
     let youngest = people[0] ? people[0].age : Infinity;
     let totalSalary = 0;
+    
+    for (const p of people) {
+        totalSalary += p.salary;
+    }
+
     for (const p of people) {
         if (p.age < youngest) youngest = p.age;
-        totalSalary += p.salary;
     }
 
     return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
