94e2f0efb2c266f0966ef31e188034c31b8c95a7
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-15 14:58:48 +0100

    chp 8.7 | Split loop | replace loop with pipe line

--- src/chp8/SplitLoop.js
+++ src/chp8/SplitLoop.js
@@ -2,19 +2,10 @@ export function reportYoungestAgeAndTotalSalary(people) {
     return `youngestAge: ${youngestAge()}, totalSalary: ${totalSalary()}`;
 
     function totalSalary() {
-        let totalSalary = 0;
-
-        for (const p of people) {
-            totalSalary += p.salary;
-        }
-        return totalSalary;
+        return people.reduce((total, person) => total + person.salary, 0);
     }
 
     function youngestAge() {
-        let youngest = people[0] ? people[0].age : Infinity;
-        for (const p of people) {
-            if (p.age < youngest) youngest = p.age;
-        }
-        return youngest;
+        return people.map(person => person.age).sort()[0];
     }
 }
