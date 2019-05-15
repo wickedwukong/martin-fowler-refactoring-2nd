6c9710865048f7579e205fdd7af94e8ca2a21c52
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-15 14:52:38 +0100

    chp 8.7 | Split loop | Use  Slide Statements to have variable youngest declared close to the loop which uses it

--- src/chp8/SplitLoop.js
+++ src/chp8/SplitLoop.js
@@ -1,11 +1,11 @@
 export function reportYoungestAgeAndTotalSalary(people) {
-    let youngest = people[0] ? people[0].age : Infinity;
     let totalSalary = 0;
-    
+
     for (const p of people) {
         totalSalary += p.salary;
     }
 
+    let youngest = people[0] ? people[0].age : Infinity;
     for (const p of people) {
         if (p.age < youngest) youngest = p.age;
     }
