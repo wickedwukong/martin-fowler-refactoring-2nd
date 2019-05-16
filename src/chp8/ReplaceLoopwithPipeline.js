39e2c3c2d60ebb32a6fedffb15cf827dfb1f6292
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-16 11:35:17 +0100

    chp 8.8 | Replace Loop with Pipeline | use map use the map operation to turn lines into an array of strings so that we can remove the split(,) in the loop

--- src/chp8/ReplaceLoopwithPipeline.js
+++ src/chp8/ReplaceLoopwithPipeline.js
@@ -1,10 +1,12 @@
 export function acquireData(input) {
     const lines = input.split("\n");
     const result = [];
-    let loopItems = lines.splice(1).filter(line => line.trim() !== "");
+    let loopItems = lines.splice(1)
+        .filter(line => line.trim() !== "")
+        .map(line => line.split(","));
     
     for (const line of loopItems) {
-        const record = line.split(",");
+        const record = line;
         if (record[1].trim() === "India") {
             result.push({city: record[0].trim(), phone: record[2].trim()});
         }
