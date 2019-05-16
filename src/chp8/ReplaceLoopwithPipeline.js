3238100388597cedb16d4f6d3045638b69b379e4
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-16 11:14:28 +0100

    chp 8.8 | Replace Loop with Pipeline | use filter to remove empty blank lines which replace the logic in the loop

--- src/chp8/ReplaceLoopwithPipeline.js
+++ src/chp8/ReplaceLoopwithPipeline.js
@@ -1,10 +1,9 @@
 export function acquireData(input) {
     const lines = input.split("\n");
     const result = [];
-    let loopItems = lines.splice(1);
+    let loopItems = lines.splice(1).filter(line => line.trim() !== "");
     
     for (const line of loopItems) {
-        if (line.trim() === "") continue;
         const record = line.split(",");
         if (record[1].trim() === "India") {
             result.push({city: record[0].trim(), phone: record[2].trim()});
