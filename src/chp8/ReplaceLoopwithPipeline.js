db114ff801d4513db4b8fe74e2d154344cec8398
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-16 11:57:16 +0100

    chp 8.8 | Replace Loop with Pipeline | use map to create records which remove the need to do so in the loop

--- src/chp8/ReplaceLoopwithPipeline.js
+++ src/chp8/ReplaceLoopwithPipeline.js
@@ -4,11 +4,12 @@ export function acquireData(input) {
     let loopItems = lines.splice(1)
         .filter(line => line.trim() !== "")
         .map(line => line.split(","))
-        .filter(lineItems => lineItems[1].trim() === "India");
-    
+        .filter(lineItems => lineItems[1].trim() === "India")
+        .map(lineItems => ({city: lineItems[0].trim(), phone: lineItems[2].trim()}));
+
     for (const line of loopItems) {
         const record = line;
-            result.push({city: record[0].trim(), phone: record[2].trim()});
+        result.push(record);
     }
     return result;
 }
