23a308c8441b7b4b8a26b61cd5eea42072dfa654
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-16 11:58:32 +0100

    chp 8.8 | Replace Loop with Pipeline | inline and cleanup

--- src/chp8/ReplaceLoopwithPipeline.js
+++ src/chp8/ReplaceLoopwithPipeline.js
@@ -1,15 +1,8 @@
 export function acquireData(input) {
     const lines = input.split("\n");
-    const result = [];
-    let loopItems = lines.splice(1)
+    return lines.splice(1)
         .filter(line => line.trim() !== "")
         .map(line => line.split(","))
         .filter(lineItems => lineItems[1].trim() === "India")
         .map(lineItems => ({city: lineItems[0].trim(), phone: lineItems[2].trim()}));
-
-    for (const line of loopItems) {
-        const record = line;
-        result.push(record);
-    }
-    return result;
 }
