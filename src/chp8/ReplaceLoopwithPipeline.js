13a5bf514b49e428b5dc6cebe9577f7e9a03890a
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-16 11:39:10 +0100

    chp 8.8 | Replace Loop with Pipeline | use filter to select only india cities. this remove the logic in the loop

--- src/chp8/ReplaceLoopwithPipeline.js
+++ src/chp8/ReplaceLoopwithPipeline.js
@@ -3,13 +3,12 @@ export function acquireData(input) {
     const result = [];
     let loopItems = lines.splice(1)
         .filter(line => line.trim() !== "")
-        .map(line => line.split(","));
+        .map(line => line.split(","))
+        .filter(lineItems => lineItems[1].trim() === "India");
     
     for (const line of loopItems) {
         const record = line;
-        if (record[1].trim() === "India") {
             result.push({city: record[0].trim(), phone: record[2].trim()});
-        }
     }
     return result;
 }
