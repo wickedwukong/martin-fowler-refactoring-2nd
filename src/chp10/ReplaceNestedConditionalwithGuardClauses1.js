5a80d4028882c410dcfb9a1b4fe95c4c9351ab85
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-17 15:27:39 +0100

    chp 10.3.1 | Replace Nested Conditional with Guard Clauses | payAmount | inline reslut variable

--- src/chp10/ReplaceNestedConditionalwithGuardClauses1.js
+++ src/chp10/ReplaceNestedConditionalwithGuardClauses1.js
@@ -1,15 +1,13 @@
 export function payAmount(employee) {
-    let result;
     if (employee.isSeparated) return {amount: 0, reasonCode: "SEP"};
     if (employee.isRetired) return {amount: 0, reasonCode: "RET"};
-    
+
     // give a dumb logic to compute amount
     // lorem.ipsum(dolor.sitAmet);
     // consectetur(adipiscing).elit();
     // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
     // ut.enim.ad(minim.veniam);
-    result = someFinalComputation();
-    return result;
+    return someFinalComputation();
 }
 
 function someFinalComputation() {
