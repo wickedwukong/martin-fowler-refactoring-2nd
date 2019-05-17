6e706dd1a4a3ac786c1c99d469d23c15ad64e42a
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-17 15:27:00 +0100

    chp 10.3.1 | Replace Nested Conditional with Guard Clauses | payAmount | change the 2ndt condition to return early so that it does not use nested condition

--- src/chp10/ReplaceNestedConditionalwithGuardClauses1.js
+++ src/chp10/ReplaceNestedConditionalwithGuardClauses1.js
@@ -1,17 +1,14 @@
 export function payAmount(employee) {
     let result;
     if (employee.isSeparated) return {amount: 0, reasonCode: "SEP"};
+    if (employee.isRetired) return {amount: 0, reasonCode: "RET"};
     
-    if (employee.isRetired) {
-        result = {amount: 0, reasonCode: "RET"};
-    } else {
-        // give a dumb logic to compute amount
-        // lorem.ipsum(dolor.sitAmet);
-        // consectetur(adipiscing).elit();
-        // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-        // ut.enim.ad(minim.veniam);
-        result = someFinalComputation();
-    }
+    // give a dumb logic to compute amount
+    // lorem.ipsum(dolor.sitAmet);
+    // consectetur(adipiscing).elit();
+    // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+    // ut.enim.ad(minim.veniam);
+    result = someFinalComputation();
     return result;
 }
 
