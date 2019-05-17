ddf65aaab3ce1ab3c002db1baa81f260a6c0736f
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-17 15:25:17 +0100

    chp 10.3.1 | Replace Nested Conditional with Guard Clauses | payAmount | change topmost condition to return ealry with using nested conditional

--- src/chp10/ReplaceNestedConditionalwithGuardClauses1.js
+++ src/chp10/ReplaceNestedConditionalwithGuardClauses1.js
@@ -1,18 +1,16 @@
 export function payAmount(employee) {
     let result;
-    if (employee.isSeparated) {
-        result = {amount: 0, reasonCode: "SEP"};
+    if (employee.isSeparated) return {amount: 0, reasonCode: "SEP"};
+    
+    if (employee.isRetired) {
+        result = {amount: 0, reasonCode: "RET"};
     } else {
-        if (employee.isRetired) {
-            result = {amount: 0, reasonCode: "RET"};
-        } else {
-            // give a dumb logic to compute amount
-            // lorem.ipsum(dolor.sitAmet);
-            // consectetur(adipiscing).elit();
-            // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-            // ut.enim.ad(minim.veniam);
-            result = someFinalComputation();
-        }
+        // give a dumb logic to compute amount
+        // lorem.ipsum(dolor.sitAmet);
+        // consectetur(adipiscing).elit();
+        // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+        // ut.enim.ad(minim.veniam);
+        result = someFinalComputation();
     }
     return result;
 }
