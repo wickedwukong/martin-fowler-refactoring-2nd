73e4f7e317fa43327facf8aeb3a6443e65fb286d
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-22 17:17:17 +0100

    chp 10.3.1 | Replace Nested Conditional with Guard Clauses | Reversing the Conditions | Consolidate Conditional Expression.

--- src/chp10/ReplaceNestedConditionalwithGuardClauses2.js
+++ src/chp10/ReplaceNestedConditionalwithGuardClauses2.js
@@ -1,9 +1,9 @@
 export function adjustedCapital(anInstrument) {
     let result = 0;
 
-    if (anInstrument.capital <= 0) return result;
-
-    if (anInstrument.interestRate <= 0 || anInstrument.duration <= 0) return result;
+    if (anInstrument.capital <= 0
+        || anInstrument.interestRate <= 0
+        || anInstrument.duration <= 0) return result;
 
     result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
 
