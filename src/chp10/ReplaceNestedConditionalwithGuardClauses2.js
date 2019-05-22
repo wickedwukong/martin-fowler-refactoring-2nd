389e702856caf9d365e70bfc3d367459d68bb7cb
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-22 17:13:48 +0100

    chp 10.3.1 | Replace Nested Conditional with Guard Clauses | Reversing the Conditions | reverse the condition to put in the guard clause.

--- src/chp10/ReplaceNestedConditionalwithGuardClauses2.js
+++ src/chp10/ReplaceNestedConditionalwithGuardClauses2.js
@@ -2,11 +2,10 @@ export function adjustedCapital(anInstrument) {
     let result = 0;
 
     if (anInstrument.capital <= 0) return result;
-    
-    if (anInstrument.capital > 0) {
-        if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
-            result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
-        }
+
+    if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+        result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
     }
+    
     return result;
 }
