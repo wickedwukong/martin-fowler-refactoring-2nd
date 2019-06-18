5e551bf057cea37668bbceec0d27550e945ce5be
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-06-18 16:58:39 +0100

    chp 11 | Separate Query from Modifier | remove the return values from the modifier

--- src/chp11/SeparateQueryFromModifier.js
+++ src/chp11/SeparateQueryFromModifier.js
@@ -14,14 +14,11 @@ export function alertForMiscreant(people, alarm) {
     for (const p of people) {
         if (p === "Don") {
             setOffAlarms(alarm, p);
-            return "Don";
         }
         if (p === "John") {
             setOffAlarms(alarm, p);
-            return "John";
         }
     }
-    return "";
 }
 
 function setOffAlarms(alarm, p) {
