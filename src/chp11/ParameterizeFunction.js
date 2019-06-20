65a4e34b7290f51936f8c8bf8e586466f7edc48d
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-06-20 15:04:37 +0100

    chp 11 | Parameterize Function | replace the call to the top band using Infinity

--- src/chp11/ParameterizeFunction.js
+++ src/chp11/ParameterizeFunction.js
@@ -3,16 +3,12 @@ export function baseCharge(usage) {
     const amount =
         withinBand(usage, 0, 100) * 0.03
         + withinBand(usage, 100, 200) * 0.05
-        + topBand(usage) * 0.07;
+        + withinBand(usage, 200, Infinity) * 0.07;
     return usd(amount);
 }
 
 function withinBand(usage, bottom, top) {
     return usage > bottom ? Math.min(usage, top) - bottom : 0;
-}
-
-function topBand(usage) {
-    return usage > 200 ? usage - 200 : 0;
 }
 
 function usd(value) {
