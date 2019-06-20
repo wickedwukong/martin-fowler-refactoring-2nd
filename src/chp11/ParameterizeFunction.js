6735d316dfcaf0c8cf6cc834989d15f3a61bb64c
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-06-20 14:51:19 +0100

    chp 11 | Parameterize Function | Start with middleBand to change it to use parameters, and then adjust other callers to fit.

--- src/chp11/ParameterizeFunction.js
+++ src/chp11/ParameterizeFunction.js
@@ -2,7 +2,7 @@ export function baseCharge(usage) {
     if (usage < 0) return usd(0);
     const amount =
         bottomBand(usage) * 0.03
-        + middleBand(usage) * 0.05
+        + withinBand(usage, 100, 200) * 0.05
         + topBand(usage) * 0.07;
     return usd(amount);
 }
@@ -11,7 +11,7 @@ function bottomBand(usage) {
     return Math.min(usage, 100);
 }
 
-function middleBand(usage) {
+function withinBand(usage, bottom, top) {
     return usage > 100 ? Math.min(usage, 200) - 100 : 0;
 }
 
