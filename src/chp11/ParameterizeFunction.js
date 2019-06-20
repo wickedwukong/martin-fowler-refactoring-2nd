2317f8dd2298215645edf918eb0905ed4978e97c
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-06-20 15:00:40 +0100

    chp 11 | Parameterize Function | replace the call to the bottom band with a call to the newly parameterized function - withinBand

--- src/chp11/ParameterizeFunction.js
+++ src/chp11/ParameterizeFunction.js
@@ -1,14 +1,10 @@
 export function baseCharge(usage) {
     if (usage < 0) return usd(0);
     const amount =
-        bottomBand(usage) * 0.03
+        withinBand(usage, 0, 100) * 0.03
         + withinBand(usage, 100, 200) * 0.05
         + topBand(usage) * 0.07;
     return usd(amount);
-}
-
-function bottomBand(usage) {
-    return Math.min(usage, 100);
 }
 
 function withinBand(usage, bottom, top) {
