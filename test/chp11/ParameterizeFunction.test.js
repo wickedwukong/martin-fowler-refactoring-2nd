68f2463ca0507799182badd968ee3ca10e44ad7c
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-06-20 13:00:29 +0100

    chp 11 | Parameterize Function | added more tests to cover bottom, middle, and top bands. We are ready to refactorg

--- test/chp11/ParameterizeFunction.test.js
+++ test/chp11/ParameterizeFunction.test.js
@@ -11,4 +11,40 @@ describe('baseCharge', () => {
         );
     });
 
+    it('should be zero when usage is less than zero', () => {
+        expect(baseCharge(-1)).to.eql({
+                currency: "$",
+                currencyName: "USD",
+                value: 0
+            }
+        );
+    });
+
+    it('bottom band', () => {
+        expect(baseCharge(50)).to.eql({
+                currency: "$",
+                currencyName: "USD",
+                value: 1.5
+            }
+        );
+    });
+
+    it('middle band', () => {
+        expect(baseCharge(100)).to.eql({
+                currency: "$",
+                currencyName: "USD",
+                value: 3
+            }
+        );
+    });
+
+    it('top band', () => {
+        expect(baseCharge(200)).to.eql({
+                currency: "$",
+                currencyName: "USD",
+                value: 8
+            }
+        );
+    });
+
 });
