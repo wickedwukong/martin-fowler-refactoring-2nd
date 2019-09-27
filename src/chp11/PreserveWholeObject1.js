6f05ab8001e0d9f53ef53ebd8a6d400fd7a350a1
Xuemin Guan <Xuemin Guan>
2019-09-27 18:14:39 +0100

    chp 11 | Preserve Whole Object 1 | remove unused code

--- src/chp11/PreserveWholeObject1.js
+++ src/chp11/PreserveWholeObject1.js
@@ -1,7 +1,5 @@
 export function temperatureAlerts(aRoom, aPlan) {
     const alerts = [];
-    const low = aRoom.daysTempRange.low;
-    const high = aRoom.daysTempRange.high;
     if (!aPlan.xxNEWwithinRange(aRoom.daysTempRange)) {
         alerts.push("room temperature went outside range");
     }
