124ee1aa89b415665020f52467a5824156781f1c
Xuemin Guan <Xuemin Guan>
2019-09-27 18:15:15 +0100

    chp 11 | Preserve Whole Object 1 | inline function on the old function

--- src/chp11/PreserveWholeObject1.js
+++ src/chp11/PreserveWholeObject1.js
@@ -12,13 +12,8 @@ export class HeatingPlan {
     constructor(temperatureRange) {
         this._temperatureRange = temperatureRange;
     }
-
-    withinRange(bottom, top) {
-        return (bottom >= this._temperatureRange.low) && (top <= this._temperatureRange.high);
-    }
-
     xxNEWwithinRange(aNumberRange) {
-        return this.withinRange(aNumberRange.low, aNumberRange.high);
+        return (aNumberRange.low >= this._temperatureRange.low) && (aNumberRange.high <= this._temperatureRange.high);
     }
 }
 
