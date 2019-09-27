3386e8cab43eb8589fda24d78bac2d33cc4dba8b
Xuemin Guan <Xuemin Guan>
2019-09-27 18:11:25 +0100

    chp 11 | Preserve Whole Object 1 | Added a the new function which takes the whole number range object and call the existing function

--- src/chp11/PreserveWholeObject1.js
+++ src/chp11/PreserveWholeObject1.js
@@ -18,5 +18,9 @@ export class HeatingPlan {
     withinRange(bottom, top) {
         return (bottom >= this._temperatureRange.low) && (top <= this._temperatureRange.high);
     }
+
+    xxNEWwithinRange(aNumberRange) {
+        return this.withinRange(aNumberRange.low, aNumberRange.high);
+    }
 }
 
