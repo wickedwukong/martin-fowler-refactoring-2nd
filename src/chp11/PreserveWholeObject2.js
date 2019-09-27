2d1d832c45ac35dd6763d68cef2bf244eea7b934
Xuemin Guan <Xuemin Guan>
2019-09-27 18:30:58 +0100

    chp 11 | Preserve Whole Object 2 | A variation | inline and renmae

--- src/chp11/PreserveWholeObject2.js
+++ src/chp11/PreserveWholeObject2.js
@@ -3,7 +3,7 @@ export function temperatureAlerts(aRoom, aPlan) {
     const alerts = [];
     let tempRange = aRoom.daysTempRange;
 
-    let isWithinRange = aPlan.xxNEWwithinRange(tempRange);
+    let isWithinRange = aPlan.withinRange(tempRange);
     if (!isWithinRange) {
         alerts.push("room temperature went outside range");
     }
@@ -17,15 +17,8 @@ export class HeatingPlan {
         this._temperatureRange = temperatureRange;
     }
 
-    withinRange(bottom, top) {
-        return (bottom >= this._temperatureRange.low) && (top <= this._temperatureRange.high);
-    }
-
-    xxNEWwithinRange(tempRange) {
-        const low = tempRange.low;
-        const high = tempRange.high;
-        let isWithinRange = this.withinRange(low, high);
-        return isWithinRange;
+    withinRange(tempRange) {
+        return (tempRange.low >= this._temperatureRange.low) && (tempRange.high <= this._temperatureRange.high);
     }
 
 }
