65d608592f194a2a7e87078a0fa4890c5c33f614
Xuemin Guan <Xuemin Guan>
2019-09-27 18:24:32 +0100

    chp 11 | Preserve Whole Object 2 | A variation | extract a new global function

--- src/chp11/PreserveWholeObject2.js
+++ src/chp11/PreserveWholeObject2.js
@@ -1,10 +1,15 @@
-export function temperatureAlerts(aRoom, aPlan) {
-    const alerts = [];
-    let tempRange = aRoom.daysTempRange;
-    
+function xxNEWwithinRange(tempRange, aPlan) {
     const low = tempRange.low;
     const high = tempRange.high;
     let isWithinRange = aPlan.withinRange(low, high);
+    return isWithinRange;
+}
+
+export function temperatureAlerts(aRoom, aPlan) {
+    const alerts = [];
+    let tempRange = aRoom.daysTempRange;
+
+    let isWithinRange = xxNEWwithinRange(tempRange, aPlan);
     if (!isWithinRange) {
         alerts.push("room temperature went outside range");
     }
