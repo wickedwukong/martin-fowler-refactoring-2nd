f695a5180976e7e1f33f4fe98c4a1df26d1c3456
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-10-24 17:37:49 +0100

    chp 11 | Replace Function with Command | move command parameters to Scorer constructor

--- src/chp11/ReplaceFunctionWithCommand.js
+++ src/chp11/ReplaceFunctionWithCommand.js
@@ -1,30 +1,36 @@
 export function score(candidate, medicalExam, scoringGuide) {
-    return new Scorer().execute(candidate, medicalExam, scoringGuide);
+  return new Scorer(candidate, medicalExam, scoringGuide).execute();
 }
 
 class Scorer {
-    execute(candidate, medicalExam, scoringGuide) {
-        let result = 0;
-        let healthLevel = 0;
-        let highMedicalRiskFlag = false;
+  constructor(candidate, medicalExam, scoringGuide) {
+    this._candidate = candidate;
+    this._medicalExam = medicalExam;
+      this._scoringGuide = scoringGuide;
+  }
 
-        if (medicalExam.isSmoker) {
-            healthLevel += 10;
-            highMedicalRiskFlag = true;
-        }
-        let certificationGrade = "regular";
-        if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-            certificationGrade = "low";
-            result -= 5;
-        }
-        // lots more code like this
-        result -= Math.max(healthLevel - 5, 0);
-        return result;
+  execute() {
+    let result = 0;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
+
+    if (this._medicalExam.isSmoker) {
+      healthLevel += 10;
+      highMedicalRiskFlag = true;
     }
+    let certificationGrade = "regular";
+    if (this._scoringGuide.stateWithLowCertification(this._candidate.originState)) {
+      certificationGrade = "low";
+      result -= 5;
+    }
+    // lots more code like this
+    result -= Math.max(healthLevel - 5, 0);
+    return result;
+  }
 }
 
 export class ScoringGuide {
-    stateWithLowCertification(state) {
-        return state < 5
-    }
+  stateWithLowCertification(state) {
+    return state < 5;
+  }
 }
