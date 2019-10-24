2d8e8a5fc61134cc38ef1e9ab2ca042de056a132
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-10-24 17:29:57 +0100

    chp 11 | Replace Function with Command | creating an empty class and then Move Function to move the function into it

--- src/chp11/ReplaceFunctionWithCommand.js
+++ src/chp11/ReplaceFunctionWithCommand.js
@@ -1,20 +1,26 @@
 export function score(candidate, medicalExam, scoringGuide) {
-    let result = 0;
-    let healthLevel = 0;
-    let highMedicalRiskFlag = false;
+    return new Scorer().execute(candidate, medicalExam, scoringGuide);
+}
 
-    if (medicalExam.isSmoker) {
-        healthLevel += 10;
-        highMedicalRiskFlag = true;
+class Scorer {
+    execute(candidate, medicalExam, scoringGuide) {
+        let result = 0;
+        let healthLevel = 0;
+        let highMedicalRiskFlag = false;
+
+        if (medicalExam.isSmoker) {
+            healthLevel += 10;
+            highMedicalRiskFlag = true;
+        }
+        let certificationGrade = "regular";
+        if (scoringGuide.stateWithLowCertification(candidate.originState)) {
+            certificationGrade = "low";
+            result -= 5;
+        }
+        // lots more code like this
+        result -= Math.max(healthLevel - 5, 0);
+        return result;
     }
-    let certificationGrade = "regular";
-    if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-        certificationGrade = "low";
-        result -= 5;
-    }
-    // lots more code like this
-    result -= Math.max(healthLevel - 5, 0);
-    return result;
 }
 
 export class ScoringGuide {
