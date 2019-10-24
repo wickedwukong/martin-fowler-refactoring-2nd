7f2cb280f9689374547c437f96296fb801a291c6
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-10-24 18:08:36 +0100

    chp 11 | Replace Function with Command | extract scoreSmoking

--- src/chp11/ReplaceFunctionWithCommand.js
+++ src/chp11/ReplaceFunctionWithCommand.js
@@ -1,36 +1,40 @@
 export function score(candidate, medicalExam, scoringGuide) {
-  return new Scorer(candidate, medicalExam, scoringGuide).execute();
+    return new Scorer(candidate, medicalExam, scoringGuide).execute();
 }
 
 class Scorer {
-  constructor(candidate, medicalExam, scoringGuide) {
-    this._candidate = candidate;
-    this._medicalExam = medicalExam;
-      this._scoringGuide = scoringGuide;
-  }
-
-  execute() {
-    this._result = 0;
-    this._healthLevel = 0;
-    this._highMedicalRiskFlag = false;
-
-    if (this._medicalExam.isSmoker) {
-      this._healthLevel += 10;
-      this._highMedicalRiskFlag = true;
+    constructor(candidate, medicalExam, scoringGuide) {
+        this._candidate = candidate;
+        this._medicalExam = medicalExam;
+        this._scoringGuide = scoringGuide;
     }
-    this._certificationGrade = "regular";
-    if (this._scoringGuide.stateWithLowCertification(this._candidate.originState)) {
-      this._certificationGrade = "low";
-      this._result -= 5;
+
+    execute() {
+        this._result = 0;
+        this._healthLevel = 0;
+        this._highMedicalRiskFlag = false;
+
+        this.scoreSmoking();
+        this._certificationGrade = "regular";
+        if (this._scoringGuide.stateWithLowCertification(this._candidate.originState)) {
+            this._certificationGrade = "low";
+            this._result -= 5;
+        }
+        // lots more code like this
+        this._result -= Math.max(this._healthLevel - 5, 0);
+        return this._result;
     }
-    // lots more code like this
-    this._result -= Math.max(this._healthLevel - 5, 0);
-    return this._result;
-  }
+
+    scoreSmoking() {
+        if (this._medicalExam.isSmoker) {
+            this._healthLevel += 10;
+            this._highMedicalRiskFlag = true;
+        }
+    }
 }
 
 export class ScoringGuide {
-  stateWithLowCertification(state) {
-    return state < 5;
-  }
+    stateWithLowCertification(state) {
+        return state < 5;
+    }
 }
