ab013c2d9edc3941e7cc0e97e1a55c0a4d378aa6
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-11-06 10:55:37 +0000

    chp 11 | Replace Command with Function | inline function and remove ChargeCalculator class

--- src/chp11/ReplaceCommandwithFunction.js
+++ src/chp11/ReplaceCommandwithFunction.js
@@ -1,10 +1,4 @@
-export class ChargeCalculator {
-    charge(customer, usage, provider) {
-        const baseCharge = customer.baseRate * usage;
-        return baseCharge + provider.connectionCharge;
-    }
-}
-
 export function charge(customer, usage, provider) {
-    return new ChargeCalculator().charge(customer, usage, provider);
+    const baseCharge = customer.baseRate * usage;
+    return baseCharge + provider.connectionCharge;
 }
