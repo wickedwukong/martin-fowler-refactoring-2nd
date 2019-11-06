1e7a03b6e8a5d09ceb75c1b6ff496f33fd787313
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-11-06 11:23:35 +0000

    chp 11 | Return Modified Value | fold the calculation into the declaration, and change the declaration to a const

--- src/chp11/ReturnModifiedValue.js
+++ src/chp11/ReturnModifiedValue.js
@@ -1,15 +1,14 @@
 export function ascentVelocity(points, totalMinutes) {
     function calculateAscent() {
-        let totalAscent = 0;
+        let result = 0;
         for (let i = 1; i < points.length; i++) {
             const verticalChange = points[i].elevation - points[i - 1].elevation;
-            totalAscent += (verticalChange > 0) ? verticalChange : 0;
+            result += (verticalChange > 0) ? verticalChange : 0;
         }
-        return totalAscent;
+        return result;
     }
 
-    let totalAscent = 0;
-    totalAscent = calculateAscent([{elevation: 10}, {elevation: 20}]);
+    const totalAscent = calculateAscent([{elevation: 10}, {elevation: 20}]);
 
     return totalAscent / totalMinutes;
 }
