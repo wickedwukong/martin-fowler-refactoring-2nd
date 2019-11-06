6a083491f1fc78a7a355fcd7dca0409b48b6da3e
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-11-06 11:22:28 +0000

    chp 11 | Return Modified Value | declare totalAscent within calculateAscent. This will make a new variable that shadows the one in the parent code

--- src/chp11/ReturnModifiedValue.js
+++ src/chp11/ReturnModifiedValue.js
@@ -1,5 +1,6 @@
 export function ascentVelocity(points, totalMinutes) {
     function calculateAscent() {
+        let totalAscent = 0;
         for (let i = 1; i < points.length; i++) {
             const verticalChange = points[i].elevation - points[i - 1].elevation;
             totalAscent += (verticalChange > 0) ? verticalChange : 0;
