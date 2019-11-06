830ce3985285dfc2649a83fbdfd83548ddb47e38
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-11-06 11:17:35 +0000

    chp 11 | Return Modified Value | begin by returning the total and showing the assignment in the caller.

--- src/chp11/ReturnModifiedValue.js
+++ src/chp11/ReturnModifiedValue.js
@@ -4,10 +4,11 @@ export function ascentVelocity(points, totalMinutes) {
             const verticalChange = points[i].elevation - points[i - 1].elevation;
             totalAscent += (verticalChange > 0) ? verticalChange : 0;
         }
+        return totalAscent;
     }
 
     let totalAscent = 0;
-    calculateAscent([{elevation: 10}, {elevation: 20}]);
+    totalAscent = calculateAscent([{elevation: 10}, {elevation: 20}]);
 
     return totalAscent / totalMinutes;
 }
